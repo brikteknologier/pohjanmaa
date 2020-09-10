802a24bd74519986d372836d4c6d6cfce76162e8
Jon Packer <contact@jonpacker.com>
2020-09-11 00:33:20 +0200

    config: allow redis db selection to be configured

--- pohjanmaa.js
+++ pohjanmaa.js
@@ -6,14 +6,17 @@ var opts = require('optimist')
   .usage('Start a pohjanmaa config server.\nUsage: $0')
   .alias('port', 'p').default('port', 2080)
   .alias('redis-port', 'R').default('redis-port', 6379)
-  .alias('redis-host', 'D').default('redis-host', 'localhost'),
+  .alias('redis-host', 'D').default('redis-host', 'localhost')
+  .alias('redis-db', 'd').default('redis-db', 0),
     argv = opts.argv;
 
 if (argv.help || argv.h) return opts.showHelp(console.log);
 
 var app = express();
 var server = module.exports = http.createServer(app);
-var redis = nredis.createClient(argv['redis-port'], argv['redis-host']);
+var redis = nredis.createClient(argv['redis-port'], argv['redis-host'], {
+  db: argv['redis-db']
+});
 
 app.use(express.bodyParser({strict: false}));
 require('./routes')(app, redis);
